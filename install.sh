e40d3063e8e7fc7d705e3825598f2579dc92387e
root <root@Users-lishaohui-Work-dev.lishaohuideMacBook-Pro.local>
2023-10-15 09:24:11 +0000

    update ycm

--- install.sh
+++ install.sh
@@ -487,57 +487,12 @@ function install_vim_plugin()
 # 安装ycm插件
 function install_ycm()
 {
-    git clone https://gitee.com/chxuan/YouCompleteMe-clang.git ~/.vim/plugged/YouCompleteMe
+    git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/plugged/YouCompleteMe
 
     cd ~/.vim/plugged/YouCompleteMe
-    distro=`get_linux_distro`
-    #read -p "Please choose to compile ycm with python2 or python3, if there is a problem with the current selection, please choose another one. [2/3] " version
-    version=2
-    if [[ $version == "2" ]]; then
-        echo "Compile ycm with python2."
-        sed -i "271ilet g:ycm_server_python_interpreter = '/usr/bin/python2.7'" ~/.vimrc
-        python2.7 ./install.py --clang-completer
-        # alpine 忽略 --clang-completer 并将 let g:ycm_clangd_binary_path 注入 .vimrc
-        #{
-        #    if [ ${distro} == "Alpine" ]; then
-        #        echo "##########################################"
-        #        echo "Apline Build, need without GLIBC."
-        #        echo "##########################################"
-        #        sed -i "273ilet g:ycm_clangd_binary_path='/usr/bin/clang'" ~/.vimrc
-        #        python2.7 ./install.py
-        #        return
-        #    fi
-        #} || {
-        #    sed -i "271ilet g:ycm_server_python_interpreter = '/usr/bin/python2.7'" ~/.vimrc
-        #    python2.7 ./install.py --clang-completer
-        #} || {
-        #    echo "##########################################"
-        #    echo "Build error, trying rebuild without Clang."
-        #    echo "##########################################"
-        #    sed -i "271ilet g:ycm_server_python_interpreter = '/usr/bin/python2.7'" ~/.vimrc
-        #    python2.7 ./install.py
-        #}
-    #else
-       ## echo "Compile ycm with python3."
-       # {
-       #     # alpine 跳过该步骤
-       #     if [ ${distro} == "Alpine" ]; then
-       #         echo "##########################################"
-       #         echo "Apline Build, need without GLIBC."
-       #         echo "##########################################"
-       #         sed -i "273ilet g:ycm_clangd_binary_path='/usr/bin/clang'" ~/.vimrc
-       #         python3 ./install.py
-       #         return
-       #     fi
-       # } || {
-       #     python3 ./install.py --clang-completer
-       # } || {
-       #     echo "##########################################"
-       #     echo "Build error, trying rebuild without Clang."
-       #     echo "##########################################"
-       #     python3 ./install.py
-       # }
-    fi
+    echo "Compile ycm with python3."
+    sed -i "271ilet g:ycm_server_python_interpreter = '/usr/bin/python3'" ~/.vimrc
+    python3 ./install.py --clangd-completer
 }
 
 # 在android上安装ycm插件
